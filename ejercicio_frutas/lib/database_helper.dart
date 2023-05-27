2ca54cc7566fbef31139ea784778ccf3a3267a52
Tulio-Rangel <tjrn20@hotmail.com>
2023-05-26 21:07:57 -0500

    added database helper

---
+++ ejercicio_frutas/lib/database_helper.dart
@@ -0,0 +1,37 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class DatabaseHelper {
+  static final DatabaseHelper _instance = DatabaseHelper.internal();
+  factory DatabaseHelper() => _instance;
+
+  static Database? _database;
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+
+    _database = await initDatabase();
+    return _database!;
+  }
+
+  DatabaseHelper.internal();
+
+  Future<Database> initDatabase() async {
+    String path = join(await getDatabasesPath(), 'fruit_database.db');
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: (Database db, int version) async {
+        await db.execute(
+          '''
+          CREATE TABLE fruits(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            description TEXT
+          )
+          ''',
+        );
+      },
+    );
+  }
+}
